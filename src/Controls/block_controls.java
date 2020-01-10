ff715807d3248c33875455a185c8d06ea849ba6b
NatetheGrate06 <58099768+NatetheGrate06@users.noreply.github.com>
2020-01-10 15:36:33 -0600

    Update block_controls.java

--- src/Controls/block_controls.java
+++ src/Controls/block_controls.java
@@ -2,15 +2,19 @@ package src.Controls;
 
 public class block_controls extends AbstractAction {
 
-  public block_controls(String text, ImageIcon icon, String desc, Integer mnomonic) {
-    super(text, icon);
-    putValue(SHORT_DESCRIPTION, desc);
-    putValue(MNEMONIC_KEY, mnemonic);
-  }
+  Action playerBlock = new AbstractAction {
+    public void actionPreformed(ActionEvent e) {
+      
+    }
+  };
+  component.getInputMap().put(Keystroke.getKeyStroke("CTRL"), 
+                                                     "Blocking");
   
-  public void playerBlock(ActionEvent e) {
-    displayResult("Blocking", e); 
+  public enum PlayerFaceDirection {
+    LEFT {
+    },
+    
+    RIGHT {
+    };
   }
-  
-  button =
 }
