724450c79a7f77c740a61e39209395785069479e
NatetheGrate06 <58099768+NatetheGrate06@users.noreply.github.com>
2020-01-10 15:03:37 -0600

    Update block_controls.java

--- src/Controls/block_controls.java
+++ src/Controls/block_controls.java
@@ -4,6 +4,11 @@ public class block_controls extends AbstractAction {
 
   public block_controls(String text, ImageIcon icon, String desc, Integer mnomonic) {
     super(text, icon);
-    
+    putValue(SHORT_DESCRIPTION, desc);
+    putValue(MNEMONIC_KEY, mnemonic);
+  }
+  
+  public void playerBlock(ActionEvent e) {
+    displayResult("Blocking", e); 
   }
 }
