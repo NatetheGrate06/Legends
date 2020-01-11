2e7b259217edb8177f4fcda4540e68551bab2485
Dakota Mcloud <59714552+DdragonI16@users.noreply.github.com>
2020-01-10 20:29:35 -0600

    Update dragon_lord.java

--- src/engine/Bosses/final_boss/dragon_lord.java
+++ src/engine/Bosses/final_boss/dragon_lord.java
@@ -1,4 +1,14 @@
 package engine.Bosses.final_boss;
-
+import com.cycling74.max.*;
+import com.cycling74.jitter.*;
+public class j51matrixinfoexample extends MaxObject {
+   public void jit_matrix(String s) 
+   {
+     JitterMatrix jm = new JitterMatrix(s);
+     outlet(0,"name", jm.getName());
+     outlet(0,"planecount", jm.getPlanecount());
+     outlet(0,"type", jm.getType());
+     outlet(0,"dim", jm.getDim());
+   
 public class dragon_lord {
 }
