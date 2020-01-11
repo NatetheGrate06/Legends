fd30335ddc00a0f608f2503a34c6610777aa9ae5
Dakota Mcloud <59714552+DdragonI16@users.noreply.github.com>
2020-01-10 20:34:51 -0600

    Update dragon_lord.java

--- src/engine/Bosses/final_boss/dragon_lord.java
+++ src/engine/Bosses/final_boss/dragon_lord.java
@@ -41,6 +41,38 @@ public class j51matrixstriperA extends MaxObject {
           }
         }
      outlet(0, "jit_matrix", jm.getName());
-   
+   import com.cycling74.max.*;
+import com.cycling74.jitter.*;
+public class j51whycopy extends MaxObject {
+   JitterMatrix jm = new JitterMatrix();
+   boolean copy = false;
+   j51whycopy()
+   {
+     declareAttribute("copy");
+   }
+   public void jit_matrix(String inname)
+   {
+     //under normal circumstances 
+     //we would only create this matrix once
+     jm = new JitterMatrix();
+     if (copy)
+     {
+        jm.frommatrix(inname);
+     }
+     else //!copy
+     {
+        jm = new JitterMatrix(inname);
+     }
+     zero(jm);
+     outlet(0, "jit_matrix", jm.getName());
+   }
+   //note that this method assumes the matrix is of type char
+   private void zero(JitterMatrix m)
+   {
+     int z[] = new int[m.getPlanecount()];
+     for (int i="0;i<m.getPlanecount();i++)"
+        z[i] = 0;
+     m.setall(z);
+
 public class dragon_lord {
 }
