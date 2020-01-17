8e3f4df35811fa82a0408bebb92dc1dab8bab923
Dakota Mcloud <59714552+DdragonI16@users.noreply.github.com>
2020-01-16 21:37:49 -0600

    Update air_magic.java

--- src/engine/Magic/air_magic.java
+++ src/engine/Magic/air_magic.java
@@ -1,18 +1,1 @@
-package engine.Magic;
 
-import engine.Characters.playable_enemy_characters.bear;
-
-public class air_magic extends enemy_character  {
-
-public Magic getbearmagic(){
-return this.bearmagic;
-
-}
-
-  private Magic main(String[]args) {
-    if(bear.isbear(true)) {
-      return bearmagic;
-    }
-    return magic;
-  }
-}
