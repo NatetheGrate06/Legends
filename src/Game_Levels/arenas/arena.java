dc37cf65f4d8224a3f709192867d270dda736759
NatetheGrate06 <58099768+NatetheGrate06@users.noreply.github.com>
2020-01-23 09:14:19 -0600

    Update arena.java

--- src/Game_Levels/arenas/arena.java
+++ src/Game_Levels/arenas/arena.java
@@ -1,4 +1,10 @@
 package Game_Levels.arenas;
 
 public class arena {
+  
+  public static arena arenas; {
+    ArrayList<arena> arenas = new ArrayList<>();
+    arenas.add(cloud_arena.cloudArena);
+    arenas.add(lava_arena.lavaArena);
+  }
 }
